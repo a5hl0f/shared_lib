1998344804df2a07039f0d054fced21c3e1f3fb7
a5hl0f <aquib7842@gmail.com>
2023-10-09 23:16:35 +0200

    shared file created

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,7 @@
+def call (Map stageParams){
+    checkout([
+            $class:'GitSCM'
+            Branches:[[name: stageParams.branch]],
+            userRemoteConfigs:[[url:stageParams.url]]
+    ])
+}
