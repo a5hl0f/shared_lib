2e6d9e773a17a2a6c407266961d7c4306354ffb2
a5hl0f <aquib7842@gmail.com>
2023-10-09 23:28:52 +0200

    mvn test created

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,5 @@
 def call (Map stageParams){
     checkout([
-            $class:'GitSCM',
-            Branches:[[name: stageParams.branch]],
             userRemoteConfigs:[[url:stageParams.url]]
     ])
 }
